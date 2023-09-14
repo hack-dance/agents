fdb96cf0584ccc00436c33555fed53e8f9e9afd7
Dimitri Kennedy <dimitrikennedy@gmail.com>
2023-09-13 22:50:02 -0400

    add hooks + update files to includei n package

--- packages/cli/src/src-files.ts
+++ packages/cli/src/src-files.ts
@@ -8,38 +8,54 @@ const __dirname = dirname(__filename);
 const hasLocalFiles = fs.existsSync(path.resolve(__dirname, "../template-files"))
 const root_path = hasLocalFiles && !process?.argv?.[1]?.includes("copy-src")
   ? path.resolve(__dirname, "../template-files")
-  : path.resolve(__dirname, "../../../packages/core/src/")
+  : path.resolve(__dirname, "../../../packages")
 
 export const everything = {
   core: {
     name: "core",
-    dependencies: ["openai", "zod", "zod-to-json-schema"],
+    dependencies: ["openai", "zod", "zod-to-json-schema", "@streamparser/json", "eventsource-parser"],
     dirs: ["utils", "ai/agents", "ai/fns"],
     files: [{
       name: "agent creators",
       targetPath: `ai/agents`,
       fileName: "index.ts",
-      srcPath: `${root_path}/ai/agents/index.ts`,
+      srcPath: `${root_path}/core/src/ai/agents/index.ts`,
     },{
       name: "OAI utils",
       targetPath: `utils`,
       fileName: "oai.ts",
-      srcPath: `${root_path}/utils/oai.ts`,
+      srcPath: `${root_path}/core/src/utils/oai.ts`,
     },{
       name: "OAI stream util",
       targetPath: `utils`,
       fileName: "oai-stream.ts",
-      srcPath: `${root_path}/utils/oai-stream.ts`,
+      srcPath: `${root_path}/core/src/utils/oai-stream.ts`,
     },{
       name: "OAI fns",
       targetPath: `ai/fns`,
       fileName: "index.ts",
-      srcPath: `${root_path}/ai/fns/index.ts`,
+      srcPath: `${root_path}/core/src/ai/fns/index.ts`,
     },{
       name: "OAI Schema FN",
       targetPath: `ai/fns`,
       fileName: "schema.ts",
-      srcPath: `${root_path}/ai/fns/schema.ts`,
+      srcPath: `${root_path}/core/src/ai/fns/schema.ts`,
+    }]
+  },
+  hooks: {
+    name: "hooks",
+    dependencies: [],
+    dirs: ["hooks"],
+    files: [{
+      name: "use-chat-stream",
+      targetPath: `hooks`,
+      fileName: "use-chat-stream.ts",
+      srcPath: `${root_path}hooks/src/hooks/use-chat-stream.ts`,
+    },{
+      name: "use-json-stream",
+      targetPath: `hooks`,
+      fileName: "use-json-stream.ts",
+      srcPath: `${root_path}hooks/src/hooks/use-json-stream.ts`,
     }]
   }
 }
