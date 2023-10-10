90b8d9b821e9414d42773cc10a24d2c299a8cf8a
Dimitri Kennedy <dimitrikennedy@gmail.com>
2023-10-10 18:27:59 -0400

    docs layout

--- apps/www/src/app/(docs)/[...slug]/layout.tsx
+++ apps/www/src/app/(docs)/[...slug]/layout.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation"
+
 import { docs } from "@/config/docs"
 import { DocHeader } from "@/components/doc-header"
 import { DocNav } from "@/components/doc-nav"
@@ -6,16 +8,20 @@ export default async function DocLayout({ children, params: { slug } }) {
   const parsedSlug = slug?.[0]?.split("#")[0]
   const packageConfig = docs[parsedSlug]
 
+  if (!packageConfig) {
+    return notFound()
+  }
+
   return (
     <>
       <DocHeader packageConfig={packageConfig} />
-      <div className="flex lg:container pt-[64px] min-h-screen">
+      <div className="flex lg:container pt-[64px] h-full flex-1 w-full">
         <aside className="top-[64px] pl-4 pt-6 min-w-[220px] h-full lg:sticky hidden lg:block">
           <DocNav packageConfig={packageConfig} />
         </aside>
 
-        <div className="w-full flex-grow pl-2 border-l-[1px] border-accent min-h-full">
-          <main className="h-screen absolute top-0 w-full overflow-y-auto">{children}</main>
+        <div className="w-full flex-grow pl-2 border-l-[1px] border-accent h-full max-w-full">
+          <main className="h-full w-full overflow-y-auto max-w-full pb-4">{children}</main>
         </div>
       </div>
     </>
