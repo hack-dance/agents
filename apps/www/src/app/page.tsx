0ba468edb974d55c004eba9a3fb745494dd7e6e0
Dimitri Kennedy <dimitrikennedy@gmail.com>
2023-10-10 18:47:35 -0400

    update namespace

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -6,10 +6,10 @@ export default async function Page() {
       <div className="text-[80px] font-bold font-blunt">×</div>
       <div className="text-left space-y-3">
         <Link href="/agents" className="text-sm font-medium leading-none block hover:underline">
-          @hack-dance/agents
+          @hackdance/agents
         </Link>
         <Link href="/hooks" className="text-sm font-medium leading-none block hover:underline">
-          @hack-dance/hooks
+          @hackdance/hooks
         </Link>
         <Link
           href="/schema-stream"
